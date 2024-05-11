3b6fa2d144ce663f2395fe3a72897221da919110
haeri-J <115974764+haeri-J@users.noreply.github.com>
2024-05-12 00:25:19 +0900

    프록시 ㄹㅇ 찐 구현 완

--- project/src/setupProxy.js
+++ project/src/setupProxy.js
@@ -1,84 +1,9 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(
-    "/login",
-    createProxyMiddleware({
-      target: 'http://localhost:8080',
-      changeOrigin: true,
-    })
-  );
+  app.use("/api", createProxyMiddleware({
+    target: 'http://localhost:8080',
+    changeOrigin: true,
+    pathRewrite: {'^/api' : ''}
+  }));
 };
-module.exports = function(app) {
-    app.use(
-      "/findID",
-      createProxyMiddleware({
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      })
-    );
-  };
-  module.exports = function(app) {
-    app.use(
-      "/findPassword",
-      createProxyMiddleware({
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      })
-    );
-  };
-  module.exports = function(app) {
-    app.use(
-      "/setPassword",
-      createProxyMiddleware({
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      })
-    );
-  };
-  module.exports = function(app) {
-    app.use(
-      "/logout",
-      createProxyMiddleware({
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      })
-    );
-  };
-  module.exports = function(app) {
-    app.use(
-      "/inquery",
-      createProxyMiddleware({
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      })
-    );
-  };
-  module.exports = function(app) {
-    app.use(
-      "/percentage",
-      createProxyMiddleware({
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      })
-    );
-  };
-  module.exports = function(app) {
-    app.use(
-      "/webcam/**",
-      createProxyMiddleware({
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      })
-    );
-  };
-  module.exports = function(app) {
-    app.use(
-      "/logout",
-      createProxyMiddleware({
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      })
-    );
-  };
-
