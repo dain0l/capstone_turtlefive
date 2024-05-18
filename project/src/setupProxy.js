84dfb81b207b1f6c22a4e955690fccfad8d48f11
haeri-J <115974764+haeri-J@users.noreply.github.com>
2024-05-18 23:42:06 +0900

    443 추가

--- project/src/setupProxy.js
+++ project/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use("/api", createProxyMiddleware({
-    target: 'httsp://docturtle.site/',
+    target: 'httsp://docturtle.site:443/',
     changeOrigin: true,
     pathRewrite: {'^/api' : ''}
   }));
