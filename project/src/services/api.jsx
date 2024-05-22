6977fc0e0103c37150555a5dbd7c2fe71101fc61
sunhyekim <kitty020103@naver.com>
2024-05-22 21:09:58 +0900

    Update api.jsx

--- project/src/services/api.jsx
+++ project/src/services/api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 // axios 인스턴스 생성
 const api = axios.create({
   //baseURL: 'http://localhost:8080', // 서버 등록시 수정 예정!!!
-  baseURL: 'https://docturtle.site/',
+  baseURL: 'https://docturtle.site',
 
   headers: {
     'Content-Type': 'application/json',
