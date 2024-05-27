735f3c07432bda9bfaab72e2731c6cdd22c3c4e8
haeri-J <115974764+haeri-J@users.noreply.github.com>
2024-05-27 22:01:51 +0900

    www로 수정완.

--- project/src/components/Examine/CameraCom.jsx
+++ project/src/components/Examine/CameraCom.jsx
@@ -34,7 +34,7 @@ function fixAlarm(){
       //Notification을 이미 허용한 사람들에게 보여주는 알람창
       new Notification('You have to fix your pose!!', {
           body: '올바른 자세를 유지해주세요.',
-          icon:"https://docturtle.site/image/turtle9.png"
+          icon:"https://www.docturtle.site/image/turtle9.png"
       });
   } else if (notificationPermission !== 'denied') {
       //Notification을 거부했을 경우 재 허용 창 띄우기
@@ -42,7 +42,7 @@ function fixAlarm(){
           if (permission === "granted") {
               new Notification('You have to fix your pose!!', {
                   body: '올바른 자세를 유지해주세요.',
-                  icon:"https://docturtle.site/image/turtle9.png"
+                  icon:"https://www.docturtle.site/image/turtle9.png"
               });
           }else {
               alert("알람 허용이 거부되었습니다.")
