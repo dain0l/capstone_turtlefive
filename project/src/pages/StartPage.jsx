8f39253932d2136d4c2ffe11d68f42e20b71acff
dain0l <123536537+dain0l@users.noreply.github.com>
2024-05-30 02:19:21 +0900

    Update StartPage.jsx

--- project/src/pages/StartPage.jsx
+++ project/src/pages/StartPage.jsx
@@ -6,7 +6,6 @@ import image1 from '../img/image1.jpg';
 import image2 from '../img/image2.jpg';
 import image3 from '../img/image3.jpg';
 import image4 from '../img/image4.jpg';
-import image5 from '../img/image5.jpg';
 
 const GlobalStyle = createGlobalStyle`
   body {
